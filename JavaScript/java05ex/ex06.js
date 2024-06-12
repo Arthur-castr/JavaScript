3b4f679dcc57ed0dc0a3b0e8e74e8663a53b2464
Arthur-castr <133271240+Arthur-castr@users.noreply.github.com>
2024-06-12 09:55:22 -0300

    Initial commit

---
+++ JavaScript/java05ex/ex06.js
@@ -0,0 +1,8 @@
+var vel = 40
+console.log(`a velocidade do seu carro é ${vel}`)
+if (vel > 60) { //condição simples
+    console.log(`a velocidade está acima do indicado!MULTADO`)
+
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
---
+++ JavaScript/java05ex/ex07.js
@@ -0,0 +1,6 @@
+var pais = 'Eua'
+if( pais = 'Br'){
+    console.log(`voce foi intendificado como Brasileiro nato pode passar!`)
+}else{
+    console.log(`voce é gringo vamos verificar seu passaporte!!`)
+}//condição normal
